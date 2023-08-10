a4d5442f8519650f72ba0976e5faca8fb837f7b9
Jung Haein <haeinj1215@gmail.com>
2023-08-10 19:03:09 +0900

    로그인/회원가입 페이지 접속 에러 수정

--- src/main/java/com/example/team_gachi/config/WebSecurityConfig.java
+++ src/main/java/com/example/team_gachi/config/WebSecurityConfig.java
@@ -1,11 +1,16 @@
 package com.example.team_gachi.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.HttpSecurityDsl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -15,5 +20,29 @@ public class WebSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf((crsf) -> crsf.disable());
+
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+                authorizeHttpRequests
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
+                        .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+
+        );
+
+        http.formLogin((formLogin) ->
+                formLogin
+                        .loginPage("/api/user/login-page").permitAll()
+                        .loginProcessingUrl("/api/user/login").permitAll()
+
+
+        );
+
+        return http.build();
     }
 }
